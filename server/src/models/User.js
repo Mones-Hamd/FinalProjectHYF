cdb54767966a929110a9ebc5a4122aa10f58f8f3
Akin <akin.tanis.dev@gmail.com>
2022-11-04 13:18:32 +0100

    validation refactor

--- server/src/models/User.js
+++ server/src/models/User.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 
 import validateAllowedFields from "../util/validateAllowedFields.js";
+import validateRequiredFields from "../util/validateRequiredFields.js";
 
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
@@ -16,32 +17,22 @@ const User = mongoose.model("users", userSchema);
 
 export const validateRegisterUser = (userObject) => {
   const errorList = [];
-  const allowedKeys = ["username", "email", "password"];
+  const requiredKeys = ["username", "email", "password"];
+  const optionalKeys = [];
 
-  const validatedKeysMessage = validateAllowedFields(userObject, allowedKeys);
+  const validatedKeysMessage = [];
+  validatedKeysMessage.push(
+    validateAllowedFields(userObject, [...requiredKeys, ...optionalKeys])
+  );
+  validatedKeysMessage.push(validateRequiredFields(userObject, requiredKeys));
 
-  if (validatedKeysMessage.length > 0) {
-    errorList.push(validatedKeysMessage);
-  }
+  validatedKeysMessage.push(validateUsername(userObject.username));
+  validatedKeysMessage.push(validateEmail(userObject.email));
+  validatedKeysMessage.push(validatePassword(userObject.password));
 
-  if (userObject.username == null) {
-    errorList.push("name is a required field");
-  } else if (!validateUsername(userObject.username)) {
-    errorList.push("Username is invalid");
-  }
-
-  if (userObject.email == null) {
-    errorList.push("email is a required field");
-  } else if (!validateEmail(userObject.email)) {
-    errorList.push("Email is invalid");
-  }
-
-  if (userObject.password == null) {
-    errorList.push("password is a required field");
-  } else if (!validatePassword(userObject.password)) {
-    errorList.push(
-      "Password must be 8 characters long and contain at least one uppercase letter, one lowercase letter, one number and one special character(#?!@$%^&*-)"
-    );
+  const messages = validatedKeysMessage.filter((msg) => !!msg);
+  if (messages.length > 0) {
+    errorList.push(messages);
   }
 
   return errorList;
@@ -49,26 +40,21 @@ export const validateRegisterUser = (userObject) => {
 
 export const validateLoginUser = (userObject) => {
   const errorList = [];
-  const allowedKeys = ["email", "password"];
+  const requiredKeys = ["email", "password"];
+  const optionalKeys = [];
 
-  const validatedKeysMessage = validateAllowedFields(userObject, allowedKeys);
+  const validatedKeysMessage = [];
+  validatedKeysMessage.push(
+    validateAllowedFields(userObject, [...requiredKeys, ...optionalKeys])
+  );
+  validatedKeysMessage.push(validateRequiredFields(userObject, requiredKeys));
 
-  if (validatedKeysMessage.length > 0) {
-    errorList.push(validatedKeysMessage);
-  }
+  validatedKeysMessage.push(validateEmail(userObject.email));
+  validatedKeysMessage.push(validatePassword(userObject.password));
 
-  if (userObject.email == null) {
-    errorList.push("email is a required field");
-  } else if (!validateEmail(userObject.email)) {
-    errorList.push("Email is invalid");
-  }
-
-  if (userObject.password == null) {
-    errorList.push("password is a required field");
-  } else if (!validatePassword(userObject.password)) {
-    errorList.push(
-      "Password must be 8 characters long and contain at least one uppercase letter, one lowercase letter, one number and one special character(#?!@$%^&*-)"
-    );
+  const messages = validatedKeysMessage.filter((msg) => !!msg);
+  if (messages.length > 0) {
+    errorList.push(messages);
   }
 
   return errorList;
@@ -76,21 +62,27 @@ export const validateLoginUser = (userObject) => {
 
 const validateUsername = (username) => {
   const regex = new RegExp(/^[A-Za-z0-9]{3,16}$/);
-  return regex.test(username);
+  if (!regex.test(username)) {
+    return "Username is invalid";
+  }
 };
 
 const validateEmail = (email) => {
   const regex = new RegExp(
     /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
   );
-  return regex.test(email);
+  if (!regex.test(email)) {
+    return "Email is invalid";
+  }
 };
 
 const validatePassword = (password) => {
   const regex = new RegExp(
     /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-.]).{8,20}$/
   );
-  return regex.test(password);
+  if (!regex.test(password)) {
+    return "Password must be 8 characters long and contain at least one uppercase letter, one lowercase letter, one number and one special character(#?!@$%^&*-)";
+  }
 };
 
 export default User;
