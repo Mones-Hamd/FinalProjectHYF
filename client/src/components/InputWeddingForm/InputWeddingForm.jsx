8a16d19690ae2f15043614489e49b04a94dd2b03
beyzagoknl <beyzagoknl@gmail.com>
2022-11-13 10:18:32 +0100

    changed components name

--- client/src/components/InputWeddingForm/InputWeddingForm.jsx
+++ client/src/components/InputWeddingForm/InputWeddingForm.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { useState } from "react";
-import "./inputWeddingForm.css";
+import "./InputWeddingForm.css";
 
 const InputWeddingForm = (props) => {
   const [focused, setFocused] = useState(false);
