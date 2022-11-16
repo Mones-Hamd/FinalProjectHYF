13c909de5c46a5cd7cacf4660daa8c548ca50f0f
Mones Hamd <mones.m.azzam91@gmail.com>
2022-11-17 00:24:48 +0100

    initial create event section home page

--- client/src/pages/HomePage/Home.jsx
+++ client/src/pages/HomePage/Home.jsx
@@ -1,12 +1,15 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "./home.css";
 import { useAuthContext } from "../../hooks/useAuthContext";
 import { useEvent } from "../../hooks/useEvent";
+import SecondaryNav from "../../components/SecondaryNav/SecondaryNav";
+import wedding from "../../Image/wedding-theme.gif";
 
 const Home = () => {
   const { user } = useAuthContext();
   const { events, getAll } = useEvent();
+  const [value, setValue] = useState("wedding");
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -14,6 +17,9 @@ const Home = () => {
 
     return () => getAll.cancel();
   }, []);
+  const handleOnClick = () => {
+    navigate("/createForm");
+  };
 
   const selectEvent = (eventId) => {
     navigate(`/event/${eventId}`);
@@ -22,12 +28,30 @@ const Home = () => {
   return (
     <div className="homePage">
       {user ? (
-        <h1>This is {user.username} home page</h1>
+        <SecondaryNav setValue={setValue} />
       ) : (
         <p>
           Something went wrong ,Normally you should not be able to see this page
         </p>
       )}
+      <div className="home-first-section">
+        {value === "wedding" ? (
+          <div className="home-first-container">
+            <div className="home-image-first">
+              <img src={wedding} alt="wedding image" />
+            </div>
+
+            <button className="button" onClick={handleOnClick}>
+              {" "}
+              Create invitation{" "}
+            </button>
+          </div>
+        ) : (
+          <div>
+            <h1>{value} event is coming soon ....</h1>
+          </div>
+        )}
+      </div>
       {getAll.isLoading && <div>loading...</div>}
       {events && (
         <div>
