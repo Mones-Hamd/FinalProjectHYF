71cb1694ce949498fe0cb670e48e50a1e544203e
Mones Hamd <mones.m.azzam91@gmail.com>
2022-11-17 10:56:03 +0100

    delete category selrctor and change button style

--- client/src/pages/HomePage/Home.jsx
+++ client/src/pages/HomePage/Home.jsx
@@ -1,15 +1,13 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import "./home.css";
 import { useAuthContext } from "../../hooks/useAuthContext";
 import { useEvent } from "../../hooks/useEvent";
-import SecondaryNav from "../../components/SecondaryNav/SecondaryNav";
 import wedding from "../../Image/wedding-theme.gif";
 
 const Home = () => {
   const { user } = useAuthContext();
   const { events, getAll } = useEvent();
-  const [value, setValue] = useState("wedding");
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -28,29 +26,23 @@ const Home = () => {
   return (
     <div className="homePage">
       {user ? (
-        <SecondaryNav setValue={setValue} />
+        <h1> Start your wedding plan with creating your invitation </h1>
       ) : (
         <p>
           Something went wrong ,Normally you should not be able to see this page
         </p>
       )}
       <div className="home-first-section">
-        {value === "wedding" ? (
-          <div className="home-first-container">
-            <div className="home-image-first">
-              <img src={wedding} alt="wedding image" />
-            </div>
+        <div className="home-first-container">
+          <div className="home-image-first">
+            <img src={wedding} alt="wedding image" />
+          </div>
 
-            <button className="button" onClick={handleOnClick}>
-              {" "}
-              Create invitation{" "}
-            </button>
-          </div>
-        ) : (
-          <div>
-            <h1>{value} event is coming soon ....</h1>
-          </div>
-        )}
+          <button className="home-create-button" onClick={handleOnClick}>
+            {" "}
+            Create invitation{" "}
+          </button>
+        </div>
       </div>
       {getAll.isLoading && <div>loading...</div>}
       {events && (
