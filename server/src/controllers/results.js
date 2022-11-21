6900b49a198822edaeedee3e89a8b1c1c4b67f69
Mones Hamd <mones.m.azzam91@gmail.com>
2022-11-21 13:52:54 +0100

    forgotten console

--- server/src/controllers/results.js
+++ server/src/controllers/results.js
@@ -53,7 +53,6 @@ export const getEventResults = async (req, res) => {
       });
     }
   } catch (err) {
-    console.log(err);
     res.status(500).send({ message: "Internal Server Error" });
   }
 };
