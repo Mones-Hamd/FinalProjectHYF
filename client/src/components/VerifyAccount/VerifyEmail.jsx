bc852470ba90e06cf717cc3b691a78351c9f7a53
beyzagoknl <beyzagoknl@gmail.com>
2022-11-24 13:41:02 +0100

    toastify message for user actions

--- client/src/components/VerifyAccount/VerifyEmail.jsx
+++ client/src/components/VerifyAccount/VerifyEmail.jsx
@@ -2,13 +2,16 @@ import React, { useEffect, useState } from "react";
 import Spinner from "../../components/Spinner/Spinner";
 import useFetch from "../../hooks/useFetch";
 import PropTypes from "prop-types";
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer, toast } from "react-toastify";
+
 const VerifyEmail = ({ userId, token }) => {
   const [message, setMessage] = useState("");
   const route = `/user/${userId}/verify/${token}`;
   const onReceived = (result) => {
     setMessage(result.message);
   };
-  const { isLoading, error, performFetch, cancelFetch } = useFetch(
+  const { isLoading, error, performFetch, cancelFetch, isSuccess } = useFetch(
     route,
     onReceived
   );
@@ -21,20 +24,22 @@ const VerifyEmail = ({ userId, token }) => {
     };
     performFetch(options);
 
+    if (error) {
+      toast.error(message, {
+        toastId: "verify-error",
+      });
+    }
+    if (isSuccess) {
+      toast.success(message, {
+        toastId: "verify-success",
+      });
+    }
     return cancelFetch;
-  }, []);
+  }, [performFetch, error, isSuccess, cancelFetch]);
   return (
     <div>
+      <ToastContainer />
       {isLoading && <Spinner />}
-      {error ? (
-        <div className="alert alert-danger" role="alert">
-          {error}
-        </div>
-      ) : (
-        <div className="alert alert-success" role="alert">
-          {message}
-        </div>
-      )}
     </div>
   );
 };
